c7740f89bfbf43d23d5d5feb340f1c2a2f1ec570
bagasaditya <bagasadit78@gmail.com>
2026-08-06 09:56:38 +0700

    fix: field dob from String to LocalDate

--- src/main/java/com/bagas/exercises/Main.java
+++ src/main/java/com/bagas/exercises/Main.java
@@ -6,23 +6,42 @@ import com.bagas.exercises.util.HibernateUtil;
 import org.hibernate.SessionFactory;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        karyawanRepository(sessionFactory);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Masukkan nama: ");
+        String nama = sc.nextLine();
+        System.out.println("Masukkan alamat: ");
+        String alamat = sc.nextLine();
+        System.out.println("Masukkan tanggal lahir: ");
+        String dob = sc.nextLine();
+
+        Karyawan karyawan = inputKaryawan(nama, alamat, dob);
+
+        karyawanRepository(sessionFactory, karyawan);
     }
 
-    public static void karyawanRepository(SessionFactory sessionFactory) {
+    public static void karyawanRepository(SessionFactory sessionFactory, Karyawan karyawan) {
         KaryawanRepository karyawanRepository = new KaryawanRepository(sessionFactory);
         karyawanRepository.findAll();
 
-        Karyawan karyawan = new Karyawan();
-        karyawan.setNama("Bagas");
-        karyawan.setAlamat("Malang");
-        karyawan.setDob(LocalDate.parse("1998-01-10"));
-        karyawan.setStatus(true);
-
         karyawanRepository.save(karyawan);
+    }
+
+    public static Karyawan inputKaryawan(String nama, String alamat, String dob) {
+        Karyawan karyawan = new Karyawan();
+        karyawan.setNama(nama);
+        karyawan.setAlamat(alamat);
+        karyawan.setDob(LocalDate.parse(dob));
+        karyawan.setStatus(true);
+        karyawan.setCreatedDate(LocalDateTime.now());
+
+        return karyawan;
     }
 }
